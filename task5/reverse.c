11476f64d8b2b00fa2ca28393e728c13163395f2
henriquesati <tpedrogatinhos@gmail.com>
2025-01-11 20:01:48 -0300

    esqueci do arquivo

--- task5/reverse.c
+++ task5/reverse.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+
+void reverse(char str[]) {
+    int init = 0;
+    int fim = strlen(str) - 1;
+    char temp;
+
+    while (init < fim) {
+        temp = str[init];
+        str[init] = str[fim];
+        str[fim] = temp;
+        init++;
+        fim--;
+    }
+}
+
+int main() {
+    char str[] = "abaxaci";  
+
+    printf("Sorigin: %s\n", str);
+
+    reverse(str);
+    printf("Srevert: %s\n", str);
+
+    return 0;
+}
